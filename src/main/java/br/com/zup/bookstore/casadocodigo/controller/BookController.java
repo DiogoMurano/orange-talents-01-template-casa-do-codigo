2fc71a6af787bdafda50818386f4f72f5af8121b
DiogoMurano <braspvp@gmail.com>
2021-01-22 11:28:50 -0300

    Finished step 0-0-6

--- src/main/java/br/com/zup/bookstore/casadocodigo/controller/BookController.java
+++ src/main/java/br/com/zup/bookstore/casadocodigo/controller/BookController.java
@@ -2,58 +2,33 @@ package br.com.zup.bookstore.casadocodigo.controller;
 
 import br.com.zup.bookstore.casadocodigo.controller.request.CreateBookRequest;
 import br.com.zup.bookstore.casadocodigo.controller.response.BookResponse;
-import br.com.zup.bookstore.casadocodigo.model.Author;
-import br.com.zup.bookstore.casadocodigo.model.Book;
-import br.com.zup.bookstore.casadocodigo.model.Category;
-import br.com.zup.bookstore.casadocodigo.repository.AuthorRepository;
-import br.com.zup.bookstore.casadocodigo.repository.BookRepository;
-import br.com.zup.bookstore.casadocodigo.repository.CategoryRepository;
+import br.com.zup.bookstore.casadocodigo.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.lang.reflect.MalformedParametersException;
-import java.util.Optional;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/book")
 public class BookController {
 
-    private final AuthorRepository authorRepository;
-    private final CategoryRepository categoryRepository;
-    private final BookRepository bookRepository;
+    private final BookService bookService;
 
     @Autowired
-    public BookController(AuthorRepository authorRepository,
-                          CategoryRepository categoryRepository, BookRepository bookRepository) {
-        this.authorRepository = authorRepository;
-        this.categoryRepository = categoryRepository;
-        this.bookRepository = bookRepository;
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
     }
 
     @PostMapping
     public ResponseEntity<BookResponse> createNewBook(@RequestBody @Valid CreateBookRequest request) {
+        return ResponseEntity.ok(bookService.createNewBook(request));
+    }
 
-        Optional<Author> optionalAuthor = authorRepository.findById(request.getAuthorId());
-
-        if(optionalAuthor.isEmpty()) {
-            throw new MalformedParametersException("Informed author was not found.");
-        }
-
-        Optional<Category> optionalCategory = categoryRepository.findById(request.getCategoryId());
-
-        if(optionalCategory.isEmpty()) {
-            throw new MalformedParametersException("Informed category was not found.");
-        }
-
-        Book book = new Book(request, optionalAuthor.get(), optionalCategory.get());
-        bookRepository.save(book);
-
-        return ResponseEntity.ok(new BookResponse(book));
+    @GetMapping
+    public List<BookResponse> listAllBooks() {
+        return bookService.listAllBooks();
     }
 
 }
---
+++ src/main/java/br/com/zup/bookstore/casadocodigo/service/BookService.java
@@ -0,0 +1,63 @@
+package br.com.zup.bookstore.casadocodigo.service;
+
+import br.com.zup.bookstore.casadocodigo.controller.request.CreateBookRequest;
+import br.com.zup.bookstore.casadocodigo.controller.response.BookResponse;
+import br.com.zup.bookstore.casadocodigo.model.Author;
+import br.com.zup.bookstore.casadocodigo.model.Book;
+import br.com.zup.bookstore.casadocodigo.model.Category;
+import br.com.zup.bookstore.casadocodigo.repository.AuthorRepository;
+import br.com.zup.bookstore.casadocodigo.repository.BookRepository;
+import br.com.zup.bookstore.casadocodigo.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.lang.reflect.MalformedParametersException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class BookService {
+
+    private final AuthorRepository authorRepository;
+    private final CategoryRepository categoryRepository;
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(AuthorRepository authorRepository,
+                       CategoryRepository categoryRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.categoryRepository = categoryRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    public BookResponse createNewBook(CreateBookRequest request) {
+        Optional<Author> optionalAuthor = authorRepository.findById(request.getAuthorId());
+
+        if (optionalAuthor.isEmpty()) {
+            throw new MalformedParametersException("Informed author was not found.");
+        }
+
+        Optional<Category> optionalCategory = categoryRepository.findById(request.getCategoryId());
+
+        if (optionalCategory.isEmpty()) {
+            throw new MalformedParametersException("Informed category was not found.");
+        }
+
+        Book book = new Book(request, optionalAuthor.get(), optionalCategory.get());
+        bookRepository.save(book);
+
+        return new BookResponse(book);
+    }
+
+    public List<BookResponse> listAllBooks() {
+        Iterable<Book> iterator = bookRepository.findAll();
+
+        List<Book> list = new ArrayList<>();
+        iterator.forEach(list::add);
+
+        return list.stream().map(BookResponse::new).collect(Collectors.toList());
+    }
+
+}
